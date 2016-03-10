f5fad152d9f7c502d0b7bffe3166cfac50aa2190
Raquel Moss <raquelxmoss@gmail.com>
2016-03-10 14:42:09 +1300

    Attempt to render some columns

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
-import {Observable} from 'rx';
-import {div} from '@cycle/dom';
+import { Observable } from 'rx';
+import { div, button } from '@cycle/dom';
+import _ from 'lodash';
+
+// Create an array of divs
+// Display them as vertical columns with flexbox
+// Start with opacity 0 and work up to 100
+// Set timeout for colour to move
+
+const columns = _.range(0, 100).map(() => {{}})
+
+const initialState = {columns}
+
+function renderColumn (column, index) {
+  return div('hi')
+}
+
+function renderColumns (columns) {
+  return columns.map((column, index) => renderColumn(column, index))
+}
 
 export default function App ({DOM}) {
+  const state$ = Observable.of(initialState)
+
   return {
-    DOM: Observable.just(div('.hello-world', 'Change me!'))
+    DOM: state$.map(state => {
+      div('.main', button('.go', "Go"))
+    })
   };
 }
